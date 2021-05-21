44d4bd440ebcf360ad185e8ebee5d9f0b6d55f9c
DEX3D <61697687+burasate@users.noreply.github.com>
2021-05-22 02:09:10 +0700

    Update profier.py

--- version/profier.py
+++ version/profier.py
@@ -1,10 +1,26 @@
 # BRS PROFILER
 # MAYA VERSION 2018-2019
+# PARALLEL MODE
 
 import json, getpass, os, time
 from csv import DictWriter
 from csv import writer
 from datetime import datetime as dt
+
+
+def getFPSUnit(*_):
+    timeUnitSet = {'game': 15, 'film': 24, 'pal': 25, 'ntsc': 30, 'show': 48, 'palf': 50, 'ntscf': 60}
+    timeUnit = cmds.currentUnit(q=True, t=True)
+    fps = 0
+
+    if timeUnit in timeUnitSet:
+        fps = int(timeUnitSet[timeUnit])
+    elif timeUnit.__contains__('fps'):
+        fps = str(cmds.currentUnit(q=True, t=True))[:-3]
+    elif timeUnit.__contains__('df'):
+        fps = str(cmds.currentUnit(q=True, t=True))[:-2]
+    fps = int(fps)
+    return fps
 
 
 def getProfiler(*_):
@@ -21,7 +37,13 @@ def getProfiler(*_):
     currFrame = cmds.currentTime(q=True)
     frameRange = 5
 
-    cmds.profiler(bufferSize=200)
+    for categoryName in cmds.profiler(q=True, ac=True):
+        categoryIndex = cmds.profiler(query=True, categoryNameToIndex=categoryName)
+    if categoryName == 'Main':
+        cmds.profiler(categoryRecording=True, categoryIndex=categoryIndex)
+    else:
+        cmds.profiler(categoryRecording=False, categoryIndex=categoryIndex)
+    cmds.profiler(bufferSize=200, reset=True)
     cmds.profiler(sampling=True)
 
     for i in range(frameRange):
@@ -42,10 +64,6 @@ def getProfiler(*_):
     timeStop = 0.0
     processTime = 0.0
     for i in range(eventCount):
-        timeStop = time.time()
-        processTime = round(abs(timeStop - timeStart), 0)
-        if drawCount > 2 and processTime > 15:
-            break
         data = {
             'eventDescription': cmds.profiler(q=True, eventDescription=True, eventIndex=i),
             'eventDuration': cmds.profiler(q=True, eventDuration=True, eventIndex=i),
@@ -53,14 +71,17 @@ def getProfiler(*_):
             'eventName': cmds.profiler(q=True, eventName=True, eventIndex=i),
             'eventStartTime': cmds.profiler(q=True, eventStartTime=True, eventIndex=i),
         }
-        if data['eventDescription'] == '':
-            continue
-        if data['eventDescription'] == 'EvaluationGraph_Normal':
+        if data['eventName'] == 'SetCurrentTime':
             drawCount += 1
         if eventMaxTime < data['eventDuration'] and data['eventName'] == 'EvaluateNode':
             eventMaxTime = data['eventDuration']
             criticalPath = data['eventDescription']
         totalDuration = data['eventStartTime']
+
+        timeStop = time.time()
+        processTime = round(abs(timeStop - timeStart), 0)
+        if drawCount > 2 and processTime > 5:
+            break
 
     totalTime = int(totalDuration / 1000)
     refreshTime = int((totalDuration / drawCount) / 1000)
@@ -83,12 +104,13 @@ def getProfiler(*_):
         'dateTime': dt.now().strftime('%Y-%m-%d %H:%M:%S'),
         'scene': raw_name,
         'user': getpass.getuser(),
-        'drawCount': drawCount,
-        'totalTime': totalTime,
-        'refreshTime': refreshTime,
+        # 'drawCount': drawCount,
+        # 'totalTime': totalTime,
+        # 'refreshTime': refreshTime,
         'frameRate': frameRate,
-        'criticalPath': criticalPath,
-        'eventMaxTime': eventMaxTime,
+        'timeUnit': getFPSUnit(),
+        # 'criticalPath': criticalPath,
+        # 'eventMaxTime': eventMaxTime,
         'processTime': processTime,
         'faceCount': cmds.polyEvaluate(cmds.ls(), face=True),
         'dagCount': len(cmds.ls(assemblies=True)),
@@ -125,5 +147,4 @@ try:
     getProfiler()
 except:
     pass
-
 
