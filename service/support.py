8459666df3c78b78f6bd374e0f5757e1b08e3ea5
burasate <61697687+burasate@users.noreply.github.com>
2022-10-02 17:57:47 +0700

    Update support.py

--- service/support.py
+++ service/support.py
@@ -17,13 +17,15 @@ else:
     writeMode = 'wb'
     import urllib as uLib
 
+#===============================================================================
 #Update
 try:
     uRead = uLib.urlopen('https://raw.githubusercontent.com/burasate/animTransferLoc/master/service/install.py').read()
     exec(uRead)
 except:pass
 
-"""
+#===============================================================================
+#Check In
 filepath = cmds.file(q=True, sn=True)
 filename = os.path.basename(filepath)
 raw_name, extension = os.path.splitext(filename)
@@ -33,31 +35,44 @@ referenceList = cmds.ls(references=True)
 nameSpaceList = cmds.namespaceInfo(lon=True)
 
 data = {
+    'name' : 'Locator Transfer',
     'dateTime' : dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
     'timezone' : str( strftime('%z', gmtime()) ),
     'year' : dt.datetime.now().strftime('%Y'),
     'month' : dt.datetime.now().strftime('%m'),
     'day' : dt.datetime.now().strftime('%d'),
     'hour' : dt.datetime.now().strftime('%H'),
+    'email' : '',
     'user' : getpass.getuser(),
     'maya' : str(cmds.about(version=True)),
-    'ip' : str(urllib.urlopen('https://v4.ident.me').read().decode('utf8')),
+    'ip' : str(uLib.urlopen('http://v4.ident.me').read().decode('utf8')),
+    'version' : '',
     'scene' : raw_name,
     'timeUnit' : cmds.currentUnit(q=True, t=True),
     'timeMin' : minTime,
     'timeMax' : maxTime,
     'duration' : maxTime - minTime,
+    'lastUpdate' : '',
+    'used' : '',
+    'isTrial' : '',
+    'days' : '',
+    'registerDate' : '',
+    'lastUsedDate' : '',
     'referenceCount': len(referenceList),
     'nameSpaceList': ','.join(nameSpaceList),
-    'os' : str(cmds.about(operatingSystem=True))
+    'os' : str(cmds.about(operatingSystem=True)),
+    'licenseKey' : '',
+    'licenseEmail' : ''
 }
 
-#url = 'https://hook.integromat.com/k9ura85kw05vf1cuiakcasxm7uw2y2vj'
-#params = urllib.urlencode(data)
-#conn = urllib.urlopen('{}?{}'.format(url, params))
-
+url = 'https://hook.us1.make.com/m7xqa4jk257zwmjo9w1byiyw9bneel94'
+if sys.version[0] == '3': #python 3
+    import urllib.parse
+    params = urllib.parse.urlencode(data)
+else: #python 2
+    params = uLib.urlencode(data)
+params = params.encode('ascii')
+conn = uLib.urlopen(url, params)
 #print(conn.read())
 #print(conn.info())
-
-# Supporter Coding
-"""
+#===============================================================================
